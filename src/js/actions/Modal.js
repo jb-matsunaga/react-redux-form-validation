fce46aafa3e03358cbbda1e92f94ceca23cfc626
jb-matsunaga <jb.matsunaga@gmail.com>
2017-06-12 22:39:14 +0900

    コメント追記

--- src/js/actions/Modal.js
+++ src/js/actions/Modal.js
@@ -1,13 +1,32 @@
-/*構文
-import * as name from "module-name";
-name
-インポートされた値を受け取るオブジェクト名
-https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/import
+/*
+https://mae.chab.in/archives/2885
+Actionは、アプリケーションからStoreにデータを送る情報のペイロード。
+Storeにおける唯一の情報源。storeのdispatch()メソッドを使って、Storeに送られる。
 */
+
 import * as ActionTypes from '../constants/ActionTypes';
+
 export function modalOpen(dec) {
     return {
         type: ActionTypes.MODAL_OPEN,
         show: dec
     }
+}
+
+/*
+▼ Actionの実装
+Actionは、アクション名（Reducerで処理を判断するため）と状態の値を持った単なるオブジェクト。
+作成したActionは、Action Creatorと呼ばれる関数の戻り値にセット。
+今回は、SENDという名前（type属性)と、Viewから受け取ったvalueという値を持ったActionを作り、
+さらにsend()関数を作って、その戻り値としてActionを返すようにする。
+*/
+// Action名の定義
+const SEND = 'SEND';
+// Action Creators
+function send(value) {
+    // Action
+    return {
+        type: SEND,
+        value
+    };
 }
--- src/js/store/reduxStore.js
+++ src/js/store/reduxStore.js
@@ -1,8 +1,34 @@
+/*
+createStore(reducer, [preloadedState], [enhancer])
+ReduxのcreateStore()メソッドを使ってstoreを作成。
+ActionとReducerをまとめるオブジェクト。
+・アプリケーションの状態(state)を保持
+・getState()メソッドを通して状態(state)へのアクセスを許可
+・dispatch(action)メソッドを通して状態(state)の更新を許可
+・subscribe(listener)メソッドを通してリスナーを登録
+・subscribe(listerner)メソッドによって返された関数を通してリスナーの登録解除をハンドリング
+*/
 import {createStore, applyMiddleware} from 'redux';
 import RootReducer from '../reducers/RootReducer';
+
 console.log('RootReducer = ' + RootReducer);
-export default function configureStore() {
-    const createStoreWithMiddleware = applyMiddleware()(createStore);
-    const store = createStoreWithMiddleware(RootReducer);
-    return store;
-}
+
+// export default function configureStore() {
+//     const createStoreWithMiddleware = applyMiddleware()(createStore);
+//     const store = createStoreWithMiddleware(RootReducer);
+//     return store;
+// }
+
+/*
+▼ Storeの実装
+createStore()メソッドの引数として、作成したReducerと初期stateを渡すようにする。
+*/
+// 初期state変数(initialState)の作成
+const initialState = {
+    value: null
+};
+// createStore()メソッドを使ってStoreの作成
+const store = createStore(RootReducer, initialState);
+console.log('store = ' + store);
+console.dir(store);
+export default store;
